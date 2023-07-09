e4f80c5def9c1b6c6c1236b1844f0957681e434c
Karthikvaradhi <karthikvaradhi6@gmail.com>
2023-07-09 13:22:22 +0530

    day 1

---
+++ Test.py
@@ -0,0 +1,25 @@
+a = "Karthik"
+c = "Suguna"
+d = " Loves "
+b= (" * " * 5)
+print (b + a + d + c + b)
+e = 57
+f = 43
+g = e*f
+print (g)
+
+h = 44
+i = 64
+j = i-h
+print (j)
+username1 = "Karthik"
+username2 = "Suguna"
+x =username1 [0:13]
+y =username2 [0:4]
+print (x)
+print (y)
+
+k = 5287545255
+j = 5287545255
+print (k >= j)
+git
